f22aad81d7937a54d4c303da7319c67d7cedf246
Jack Herrington <jherr@pobox.com>
2024-09-25 20:49:47 -0700

    Adding tanstack router

--- vinxi-spa/home/index.tsx
+++ vinxi-spa/home/index.tsx
@@ -1,9 +1,50 @@
 /// <reference types="vinxi/types/client" />
 import ReactDOM from "react-dom/client";
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "@tanstack/react-router";
 
 import { Button } from "../components/ui/button";
 
 import "./index.css";
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  ),
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: () => <ImageDisplay />,
+});
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/about",
+  component: () => <div>About</div>,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = createRouter({ routeTree, defaultPreload: "intent" });
 
 function ImageDisplay() {
   return (
@@ -15,4 +56,6 @@ function ImageDisplay() {
   );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<ImageDisplay />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
